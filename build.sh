c054835f5c729a951520105eb95569eda153b32a
Nick Schuch <nick@myschuch.com>
2014-12-31 22:53:11 +1000

    Add the build script, update drush make.

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Name:        build.sh
+# Description: Provides a Drush make build script.
+
+BUILD=/opt/drupal 		# Directory that the build pack uploaded configuration to.
+WEBROOT=/var/www		# Root of the uploaded project code.
+DOCROOT=$WEBROOT/app	# Application directory of the project.
+MAKEFILE=drupal.make 	# Name of the drush make file that we will build the application with.
+
+# Build the code base.
+drush make $WEBROOT/$MAKEFILE $DOCROOT
+
+# Setup configuration files.
+scp $BUILD/conf/drupal/settings.php $DOCROOT/sites/default/settings.php
+if [ -f "${WEBROOT}/settings.php" ]; then
+	mv $WEBROOT/settings.php $DOCROOT/sites/default/settings.overrides.php
+fi
+
+# Create a directory structure.
+mkdir -p $DOCROOT/sites/default/files/tmp
+mkdir -p $DOCROOT/sites/default/private
+chown -R www-data:www-data $DOCROOT/sites/default/files
+chown -R www-data:www-data $DOCROOT/sites/default/private
--- drupal.make
+++ drupal.make
@@ -5,4 +5,12 @@ core = 7.x
 projects[drupal][version] = 7.32
 
 ; Contrib modules.
+projects[libraries][subdir] = "contrib"
+projects[awssdk][subdir] = "contrib"
 projects[amazons3][subdir] = "contrib"
+
+; Libraries.
+libraries[awssdk][download][type] = "get"
+libraries[awssdk][download][url] = "http://pear.amazonwebservices.com/get/sdk-1.6.2.tgz"
+libraries[awssdk][directory_name] = "awssdk"
+libraries[awssdk][destination] = "libraries/awssdk"
