030d2108b24c9f40b7b481ec8039b51cec44296b
Nick Schuch <nick@myschuch.com>
2015-01-01 18:49:25 +1000

    Update drush make build flow.

--- build.sh
+++ build.sh
@@ -3,20 +3,23 @@
 # Name:        build.sh
 # Description: Provides a Drush make build script.
 
-BUILD=/opt/drupal 		# Directory that the build pack uploaded configuration to.
-WEBROOT=/var/www		# Root of the uploaded project code.
-DOCROOT=$WEBROOT/app	# Application directory of the project.
-MAKEFILE=drupal.make 	# Name of the drush make file that we will build the application with.
+BUILD=/opt/drupal               # Directory that the build pack uploaded configuration to.
+WEBROOT=/var/www                # Root of the uploaded project code.
+DOCROOT=$WEBROOT/app    # Application directory of the project.
+MAKEFILE=drupal.make    # Name of the drush make file that we will build the application with.
 
 # Build the code base.
+rm -fR $DOCROOT
 drush make $WEBROOT/$MAKEFILE $DOCROOT
-ln -s $WEBROOT/modules $DOCROOT/sites/all/modules
-ln -s $WEBROOT/themes $DOCROOT/sites/all/themes
+mkdir -p $DOCROOT/sites/all/modules
+mkdir -p $DOCROOT/sites/all/themes
+rsync -avz $WEBROOT/modules $DOCROOT/sites/all/modules/custom
+rsync -avz $WEBROOT/themes $DOCROOT/sites/all/themes/custom
 
 # Setup configuration files.
 scp $BUILD/conf/drupal/settings.php $DOCROOT/sites/default/settings.php
 if [ -f "${WEBROOT}/settings.php" ]; then
-	mv $WEBROOT/settings.php $DOCROOT/sites/default/settings.overrides.php
+        mv $WEBROOT/settings.php $DOCROOT/sites/default/settings.overrides.php
 fi
 
 # Create a directory structure.
--- drupal.make
+++ drupal.make
@@ -1,16 +1,17 @@
 api = 2
 core = 7.x
 
-; Drupal core.
-projects[drupal][version] = 7.32
+; Core.
+projects[drupal] = 7.34
 
-; Contrib modules.
+; Contrib.
 projects[libraries][subdir] = "contrib"
 projects[awssdk][subdir] = "contrib"
 projects[amazons3][subdir] = "contrib"
 
 ; Libraries.
-libraries[awssdk][download][type] = "get"
-libraries[awssdk][download][url] = "http://pear.amazonwebservices.com/get/sdk-1.6.2.tgz"
-libraries[awssdk][directory_name] = "awssdk"
-libraries[awssdk][destination] = "libraries/awssdk"
+projects[awssdk_lib][name] = "awssdk"
+projects[awssdk_lib][type] = library
+projects[awssdk_lib][download][type] = git
+projects[awssdk_lib][download][url] = https://github.com/amazonwebservices/aws-sdk-for-php.git
+projects[awssdk_lib][download][tag] = 1.5.10
